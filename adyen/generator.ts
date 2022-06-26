515ab73e365b3f3e80e814b3e756df5acbc2759e
Andrey Petrov <andrey5608@gmail.com>
2022-06-26 17:05:40 +0200

    Added hmac calculation and webhook data class

--- adyen/generator.ts
+++ adyen/generator.ts
@@ -1,71 +1,60 @@
 import moment = require('moment');
+import { calculateHmac } from './hmac';
+import { WebhookData } from './webhook-data';
 
 export function generateWebhook() {
-  console.log('adyen');
-  let event = generateEvent(
-    'CSC8CDFFFFFFFFFFFF',
-    'YmxhaGJsYWhobWFjMTExMzMz',
-    1001,
-    moment().unix(),
-    'BlahBlahGmbHECOM',
-    '024b2fc4-b2d7-456f-ada0-51a4554ff60e:9999',
-    'visa',
-    'BADCASDSDASDASDS',
-    true,
-    'EUR',
-    '03/2030',
-    '067000',
-    '1111'
-  );
-  console.log(JSON.stringify(event));
-  return event;
+    console.log('adyen');
+    let webhookData = new WebhookData({ amountInCents: 1111 });
+    let event = generateEvent(webhookData);
+    console.log(JSON.stringify(event));
+    return event;
 }
 
-function generateEvent(
-  checkoutSessionId: string,
-  hmacSignature: string,
-  amountInCents: number,
-  unixEventDatetime: number,
-  merchantAccountCode: string,
-  merchantReference: string,
-  paymentMethod: string,
-  pspReference: string,
-  executionResult: boolean = true,
-  currency: string = 'EUR',
-  expiryDate: string = '03/2030',
-  authCode: string = '067000',
-  cardLast4: string = '1111'
-) {
-  return {
-    live: 'false',
-    notificationItems: [
-      {
-        NotificationRequestItem: {
-          additionalData: {
-            expiryData: expiryDate,
-            authCode: authCode,
-            cardSummary: cardLast4,
-            'threeds2.cardEnrolled': 'false',
-            checkoutSessionId: checkoutSessionId,
-            hmacSignature: hmacSignature,
-          },
-          amount: {
-            currency: currency,
-            value: amountInCents,
-          },
-          eventCode: 'AUTHORISATION',
-          eventDate: moment.unix(unixEventDatetime).format('YYYY-MM-DDTHH:mm:ssZ'),
-          merchantAccountCode: merchantAccountCode,
-          merchantReference: merchantReference,
-          operations: ['CANCEL', 'CAPTURE', 'REFUND'],
-          paymentMethod: paymentMethod,
-          pspReference: pspReference,
-          reason: `${authCode}:${cardLast4}:${expiryDate}`,
-          success: executionResult,
-        },
-      },
-    ],
-  };
+function generateEvent(webhookData: WebhookData) {
+    let isSuccessful = webhookData.executionResult.toString().toLowerCase();
+    let hmac = calculateHmac(
+        webhookData.adyenHmacKey,
+        webhookData.pspReference,
+        webhookData.originalReference,
+        webhookData.merchantAccountCode,
+        webhookData.merchantReference,
+        webhookData.amountInCents,
+        webhookData.currency,
+        isSuccessful
+    );
+
+    return {
+        live: 'false',
+        notificationItems: [
+            {
+                NotificationRequestItem: {
+                    additionalData: {
+                        expiryData: webhookData.expiryDate,
+                        authCode: webhookData.authCode,
+                        cardSummary: webhookData.cardLast4,
+                        'threeds2.cardEnrolled': 'false',
+                        checkoutSessionId: webhookData.checkoutSessionId,
+                        hmacSignature: hmac,
+                    },
+                    amount: {
+                        currency: webhookData.currency,
+                        value: webhookData.amountInCents,
+                    },
+                    eventCode: 'AUTHORISATION',
+                    eventDate: moment
+                        .unix(webhookData.unixEventDatetime)
+                        .format('YYYY-MM-DDTHH:mm:ssZ'),
+                    merchantAccountCode: webhookData.merchantAccountCode,
+                    merchantReference: webhookData.merchantReference,
+                    operations: ['CANCEL', 'CAPTURE', 'REFUND'],
+                    paymentMethod: webhookData.paymentMethod,
+                    pspReference: webhookData.pspReference,
+                    reason: `${webhookData.authCode}:${webhookData.cardLast4}:${webhookData.expiryDate}`,
+                    success: isSuccessful,
+                },
+            },
+        ],
+    };
 }
 
 generateWebhook(); // TODO remove
