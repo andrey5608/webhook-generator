9ba0f4e8a773b464b5293a5112f2cd2a3f323b04
Andrei <andrey5608@gmail.com>
2023-01-21 19:55:26 +0100

    Hmac calculation fix

--- ivy/hmac.ts
+++ ivy/hmac.ts
@@ -2,6 +2,6 @@ import { createHmac } from 'crypto';
 
 export function calculateHmac(data: any, key: string){
     const hmac = createHmac('sha-256', key);
-    hmac.update(JSON.stringify(data));
+    hmac.update(JSON.stringify(data, null, 0));
     return hmac.digest('hex');
 }
